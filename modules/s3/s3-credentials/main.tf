74661d8c9bcbd187ea0cdb0e387d1070899f9203
Maksim Kuznetsov <kuznetsov.m@selectel.ru>
2024-03-28 19:23:30 +0300

    rm unnecessary

--- modules/s3/s3-credentials/main.tf
+++ modules/s3/s3-credentials/main.tf
@@ -1,10 +1,3 @@
-module "selectel_token" {
-  source      = "../../selectel-token"
-  os_account  = var.os_account
-  os_password = var.os_password
-  os_username = var.os_username
-}
-
 resource "selectel_iam_s3_credentials_v1" "s3_credentials_1" {
   name       = var.credentials_name
   user_id    = var.os_user_id
