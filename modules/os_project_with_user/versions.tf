a2d9a7d9b1562b53cd752be0f5156e66ba0717e8
Filipp Fedorov <fedorov.f@selectel.ru>
2024-04-24 18:35:26 +0300

    bump selectel/selectel provider version

--- modules/os_project_with_user/versions.tf
+++ modules/os_project_with_user/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     selectel = {
       source  = "selectel/selectel"
-      version = ">= 4.0.1"
+      version = "5.0.2"
     }
     random = {
       source  = "hashicorp/random"
