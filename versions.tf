e3106deb26c6fc4ad4a3664752a2124f6263d9c5
Filipp Fedorov <fedorov.f@selectel.ru>
2024-05-28 11:39:56 +0300

    bump root terraform provider selectel to latest 5.1.1

--- versions.tf
+++ versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     selectel = {
       source  = "selectel/selectel"
-      version = "5.0.2"
+      version = "5.1.1"
     }
     openstack = {
       source  = "terraform-provider-openstack/openstack"
       version = "1.53.0"
     }
   }
-  backend "http" {}
+  backend "s3" {}
   required_version = ">= 1.0.0"
 }
