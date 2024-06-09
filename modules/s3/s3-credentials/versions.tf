ebb3b58e64011b998dfb827227e98014f73c29c8
Filipp Fedorov <fedorov.f@selectel.ru>
2024-06-09 20:12:15 +0300

    bump selectel provider version to 5.1.1

--- modules/s3/s3-credentials/versions.tf
+++ modules/s3/s3-credentials/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     selectel = {
       source  = "selectel/selectel"
-      version = ">=5.0.2"
+      version = ">=5.1.1"
     }
   }
   required_version = ">= 1.0.0"
