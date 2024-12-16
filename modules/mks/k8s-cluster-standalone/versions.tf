59e3b5ad73641f12ca637cccfe0f4113a3b9b8af
Alexey Orlov <orlov.aa@selectel.com>
2024-12-16 12:30:37 +0300

    updated openstack provider version reqs

--- modules/mks/k8s-cluster-standalone/versions.tf
+++ modules/mks/k8s-cluster-standalone/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     selectel = {
       source  = "registry.terraform.io/selectel/selectel"
-      version = ">=5.0.2"
+      version = ">= 5.0.2"
     }
     openstack = {
       source  = "registry.terraform.io/terraform-provider-openstack/openstack"
-      version = "1.53.0"
+      version = "~> 3.0.0"
     }
   }
 }
