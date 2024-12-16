c444bf5a0cadb33eeac91171c974b52eaf5c7775
Alexey Orlov <orlov.aa@selectel.com>
2024-12-16 12:23:40 +0300

    changed backend s3 params

--- versions.tf
+++ versions.tf
@@ -10,8 +10,12 @@ terraform {
     }
   }
   backend "s3" {
-    endpoints = {
-      s3 = "s3.ru-1.storage.selcloud.ru"
-    }
+    endpoints                   = { s3 = "https://s3.ru-1.storage.selcloud.ru" }
+    region                      = "ru-1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
   }
 }
